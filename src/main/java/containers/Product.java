7de9d7b632af74a8b209ff0854707915f1af86d6
Pplociennik <przemx.84@gmail.com>
2018-09-16 00:03:16 +0200

    First commit; containers added

---
+++ src/main/java/containers/Product.java
@@ -0,0 +1,20 @@
+package containers;
+
+public class Product {
+
+    private String productName;
+    private double productPrize;
+
+    public Product(String productName, double productPrize) {
+        this.productName = productName;
+        this.productPrize = productPrize;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrize() {
+        return productPrize;
+    }
+}
---
+++ src/main/java/users/Profile.java
@@ -0,0 +1,22 @@
+package users;
+
+public class Profile {
+
+    /* counters */
+    private int periodsCounter;
+    private int weeksCounter;
+    private int productsCounter;
+
+
+    public int getPeriodsCounter() {
+        return periodsCounter;
+    }
+
+    public int getWeeksCounter() {
+        return weeksCounter;
+    }
+
+    public int getProductsCounter() {
+        return productsCounter;
+    }
+}
