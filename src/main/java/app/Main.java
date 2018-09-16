c69a1a163645621f4e832356672d1a0616994dce
Pplociennik <przemx.84@gmail.com>
2018-09-16 10:43:24 +0200

    saving and reading (Json) profile added

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,21 @@
 package app;
 
+import users.Profile;
+import users.ProfileService;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
     System.out.println("Tutaj będzie startować aplikacja!!");
+
+        Profile profile = new Profile("PSZEMEG");
+        ProfileService profileService = new ProfileService();
+        try {
+            profileService.saveProfile(profile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/users/ProfileService.java
+++ src/main/java/users/ProfileService.java
@@ -1,4 +1,34 @@
 package users;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ProfileService {
+
+    public ProfileService() {
+
+    }
+
+    public void saveProfile(Profile userProfile) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.writeValue(new File(userProfile.getProfileName() + ".json"), userProfile);
+    }
+
+    public void readProfile(Profile userProfile) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            mapper.readValue(userProfile.getProfileName() + ".json", new TypeReference<Profile>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
